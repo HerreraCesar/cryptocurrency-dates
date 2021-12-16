d3ce04b35edcc4c3bcb4a57b043d9028334aa587
Herrera <herrera.cesar.arg@outlook.com>
2021-12-16 19:21:14 -0300

    Load

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
-
-var datos = $.getJSON( "https://api.binance.com/api/v3/ticker/24hr")
-datos.done(function(datos) {
-    console.log(datos);
-    datos.forEach(money => {
-        $('table').append(`
-            <tr>
-                <td>${money.symbol}</td>
-                <td>${money.lastPrice}</td>
-                <td>${money.priceChangePercent}</td>
-            </tr>
-        `)
-    });
+window.addEventListener("load", function() {
+    var datos = $.getJSON( "https://api.binance.com/api/v3/ticker/24hr")
+    datos.done(function(datos) {
+        console.log(datos);
+        datos.forEach(money => {
+            $('table').append(`
+                <tr>
+                    <th>TICKER</th>
+                    <th>PRECIO</th>
+                    <th>24 HS.</th>
+                </tr>
+                <tr>
+                    <td>${money.symbol}</td>
+                    <td>${money.lastPrice}</td>
+                    <td>${money.priceChangePercent}</td>
+                </tr>
+            `)
+        });
+    })
 })
